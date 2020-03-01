06a410eff055e128802b4d346bfbbc16ecfbfbd4
Domenico Maisano <dev.dmaisano@gmail.com>
2020-02-29 21:26:39 -0500

    rendered list

--- udemy-react-tutorial/src/App.tsx
+++ udemy-react-tutorial/src/App.tsx
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
+import { Person, PersonProps } from './Person/Person';
 
 export class App extends Component {
+  state: {
+    persons: PersonProps[];
+  } = {
+    persons: [
+      {
+        name: 'Finn',
+        age: 17,
+      },
+      {
+        name: 'Jake',
+        age: 34,
+      },
+      {
+        name: 'Fern',
+        age: 17,
+      },
+    ],
+  };
+
   render() {
+    const { persons } = this.state;
+
     return (
       <div className="App">
-        <p>foo</p>
+        {persons.map((person) => (
+          <Person {...person} />
+        ))}
       </div>
     );
   }
