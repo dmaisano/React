08a8b59c687e2110f0294869597c4b4b374bc7f8
Domenico Maisano <dev.dmaisano@gmail.com>
2020-03-01 22:33:09 -0500

    using react hooks

--- udemy-react-tutorial/src/Person/Person.tsx
+++ udemy-react-tutorial/src/Person/Person.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-export interface PersonProps {
+interface Props {
   name: string;
   age: number;
   children?: any;
 }
 
-export const Person: React.FC<PersonProps> = (props: PersonProps) => {
+export const Person: React.FC<Props> = (props: Props) => {
   return (
     <p>
       My name is {props.name} and I am {props.age} years old
