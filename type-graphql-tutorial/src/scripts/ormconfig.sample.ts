07a5ace7dd46821da0e1096a4d9c6b14b30ee1b5
Domenico Maisano <dmaisanooo@gmail.com>
2020-03-29 21:16:24 -0400

    added setup script

---
+++ type-graphql-tutorial/src/scripts/ormconfig.sample.ts
@@ -0,0 +1,13 @@
+import { ConnectionOptions } from "typeorm";
+
+export const typeOrmConfig: ConnectionOptions = {
+  type: "mysql",
+  host: "",
+  port: 0,
+  username: "",
+  password: "",
+  database: "",
+  synchronize: true,
+  logging: true,
+  entities: [""],
+};
---
+++ type-graphql-tutorial/src/scripts/setup.ts
@@ -0,0 +1,38 @@
+import { promises as fsPromises } from "fs";
+import { resolve } from "path";
+import chalk from "chalk";
+
+const { readFile, writeFile } = fsPromises;
+
+/**
+ * Initial project setup script
+ */
+const main = async () => {
+  createOrmConfig("src/ormconfig.ts");
+  createOrmConfig("src/ormconfig.test.ts");
+};
+
+const createOrmConfig = async (fileName: string) => {
+  const fileContents = (await readFile(`${__dirname}/ormconfig.sample.ts`, {
+    encoding: "utf-8",
+  })) as string;
+
+  const absPath = chalk.magenta(resolve(fileName));
+
+  try {
+    await writeFile(fileName, fileContents, {
+      encoding: "utf-8",
+      flag: "wx",
+    });
+
+    console.log(`${chalk.green("Created file:")} ${absPath}`);
+  } catch (e) {
+    if (e!.errno === -17) {
+      console.log(`${chalk.red("File already exists:")} ${absPath}`);
+    } else {
+      console.log(`${chalk.red("Unable to create file:")} ${absPath}`);
+    }
+  }
+};
+
+main();
